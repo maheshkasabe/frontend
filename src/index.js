a3a87d17a45440d580a16593d98ed0613324a67c
Mahesh <maheshkasbe010@gmail.com>
2023-04-07 20:16:15 +0530

    last

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { ContextProvider } from './SocketContext';
-
-ReactDOM.render(<ContextProvider> <App /> </ContextProvider>,document.getElementById('root'));
-
-
-
